cdf9d9d63b8464422d24daa6d5a47dde8ff6cc64
Vlad-dos <vlad082006@yandex.ru>
2014-05-01 15:50:38 +0600

    New
    Creates new project

---
+++ Labirinth/Assets/Scripts/level_administration.js
@@ -0,0 +1,87 @@
+﻿#pragma strict
+
+// Режимы меню:
+enum menu_mode {
+    start,
+    client,
+    server,
+    game
+}	
+
+//buttons
+var server_button : GUI_location;
+var client_button : GUI_location;
+var main_window : GUI_location;
+var name_field : GUI_scale;
+var back_button : GUI_location;
+var gui_skin : GUISkin;
+
+var mode : menu_mode;
+var player_name : String;
+
+function Start() {
+    DontDestroyOnLoad(gameObject);
+}
+
+var style : GUIStyle;
+
+function OnGUI() {
+    //Начальное меню:
+    if (mode == menu_mode.start) { 
+        GUI.Box(main_window.position, main_window.content, gui_skin.window);
+     	player_name = GUI.TextField(name_field.Convert_Scales(), player_name, 28, gui_skin.textField);
+        if (GUI.Button(server_button.position, server_button.content, gui_skin.button)) {
+            mode = menu_mode.server;
+        }
+        if (GUI.Button(client_button.position, client_button.content, gui_skin.button)) {
+            mode = menu_mode.client;
+        }
+    }
+    //Меню Сервера и игрока
+    if (mode == menu_mode.client) {
+        gameObject.GetComponent(client_script).enabled = true;
+        if (GUI.Button(back_button.position, back_button.content, gui_skin.button)) {
+            mode = menu_mode.start;
+            gameObject.GetComponent(client_script).enabled = false;
+        }
+    }
+    if (mode == menu_mode.server) {
+        gameObject.GetComponent(server_script).enabled = true;
+        if (GUI.Button(back_button.position, back_button.content, gui_skin.button)) {
+            mode = menu_mode.start;
+            gameObject.GetComponent(server_script).enabled = false;
+        }
+    }
+    if (mode == menu_mode.game) {
+//      Debug.Log("lol");
+    }
+}
+
+function Update() {
+    //Set buttons params//
+    server_button.position = server_button.scale.Convert_Scales();
+    server_button.normalize_font();
+    main_window.position = main_window.scale.Convert_Scales();
+    main_window.normalize_font();
+    client_button.position = client_button.scale.Convert_Scales();
+    client_button.normalize_font();
+    back_button.position = back_button.scale.Convert_Scales();
+    back_button.normalize_font();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Labirinth/Assets/Scripts/server_script.js
@@ -0,0 +1,54 @@
+ // Скрипт для сервера.
+var start_msg : String = "Click to start";
+var running_msg : String = "Waiting for oponent";
+private var msg : String;
+var label : GUI_location;
+var start_button : GUI_location;
+var map_select : GUI_location;
+var map_number : int;
+var maps : String[];
+var gui_skin : GUISkin;
+
+function OnGUI(){
+	var main_window : GUI_location = GetComponent(level_administration).main_window;
+	GUI.Box(main_window.position, main_window.content, gui_skin.window);
+	start_button.position = start_button.scale.Convert_Scales();
+	start_button.normalize_font();
+    map_select.position = map_select.scale.Convert_Scales();
+    map_select.normalize_font();
+	label.position = label.scale.Convert_Scales();
+	label.normalize_font();
+	label.content.text = msg;
+	GUI.Label(label.position, label.content, gui_skin.textField);
+
+	if (Network.peerType == Network.peerType.Disconnected) {
+		start_button.content.text = "Start";
+		if (GUI.Button(start_button.position, start_button.content, gui_skin.button)) {
+			Network.InitializeServer(10,25000,false); 
+		}
+	}
+
+	if (Network.peerType != Network.peerType.Disconnected) {
+		msg= running_msg;
+		start_button.content.text = "OFF";
+		if (GUI.Button(start_button.position, start_button.content, gui_skin.button)) {
+			Network.Disconnect();
+			msg= start_msg;
+		}
+	}
+   
+    map_number = GUI.Toolbar(map_select.position, map_number, maps, gui_skin.button);
+} 
+
+function Start() {
+	msg = start_msg;
+}
+
+function Update() {
+}
+
+@RPC
+function get_level() {
+    Debug.Log("catched");
+    networkView.RPC("ready", RPCMode.All, maps[map_number]);
+}
