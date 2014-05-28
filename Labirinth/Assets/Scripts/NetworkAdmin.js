419bb0f81193b12b4e85e5811166175cc05e8d76
Vlad-dos <vlad082006@yandex.ru>
2014-05-28 23:28:30 +0600

    New Client-Server system

---
+++ Labirinth/Assets/Scripts/NetworkAdmin.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var playerName : String = "NooB"; 
+var serverPrefab : GameObject;
+
+function Start () {
+	DontDestroyOnLoad(gameObject);
+}
+
+function LaunchServer(w : int, h : int, map : String) {
+	var server : GameObject = Instantiate(serverPrefab);
+	server.GetComponent(Server).initField(w, h);
+	Application.LoadLevel(map);
+}
+
---
+++ Labirinth/Assets/Scripts/Server.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var field : Labyrinth;
+
+function Start () {
+	DontDestroyOnLoad(gameObject);
+	Network.InitializeServer(32, 25000, false);
+}
+
+function initField(w : int, h : int) {
+	field = new Labyrinth(w, h);
+}
+
+function OnPlayerDisconnected (player : NetworkPlayer) {
+	Network.RemoveRPCs(player, 0);
+	Network.DestroyPlayerObjects(player);
+}
